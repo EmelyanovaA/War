0cc073b7025a8c7d651af27ed442a8f13821ecbc
andrew-a-gusev <andrew.a.gusev@gmail.com>
2016-10-25 13:39:08 +0300

    Update Archer.java

--- src/Archer.java
+++ src/Archer.java
@@ -2,8 +2,9 @@
 class Archer extends Unit implements Fighter, Movable {
 
     @Override
-    public void attack() {
+    public void attack(Unit unit) {
         Attack a = new Attack(20);
+        a.attack(unit);
     }
 
     @Override
